9fe28a1f45fdeb35b433fc50ede14f83b6dbac92
Madjinda Lucas <lucasmadjinda@gmail.com>
2024-04-05 16:52:04 +0200

    modification du défis

--- 5_Exercices_En_Cours_Formation/Exo_Algo/Pydefis/herculito1_lion_némée/main.js
+++ 5_Exercices_En_Cours_Formation/Exo_Algo/Pydefis/herculito1_lion_némée/main.js
@@ -1,46 +1,48 @@
-let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-let god = "ARTEMIS ASCLEPIOS ATHENA ATLAS CHARON CHIRON CRONOS DEMETER EOS ERIS EROS GAIA HADES HECATE HEPHAISTOS HERA HERMES HESTIA HYGIE LETO MAIA METIS MNEMOSYNE NYX OCEANOS OURANOS PAN PERSEPHONE POSEIDON RHADAMANTHE SELENE THEMIS THETIS TRITON ZEUS "
+let god =
+	"ARTEMIS ASCLEPIOS ATHENA ATLAS CHARON CHIRON CRONOS DEMETER EOS ERIS EROS GAIA HADES HECATE HEPHAISTOS HERA HERMES HESTIA HYGIE LETO MAIA METIS MNEMOSYNE NYX OCEANOS OURANOS PAN PERSEPHONE POSEIDON RHADAMANTHE SELENE THEMIS THETIS TRITON ZEUS ";
 
- let sommelettre=0;
- let nom=""
- let objet={
-    name: "",
-    somme:"",
+let sommelettre = 0;
+let nom = "";
+let objet = {
+	name: "",
+	somme: "",
 };
-let array=[];
-let trie="";
-for(let i=0; i<god.length;i++){
-    if(god[i]==" "){                    // chaque espace de god
-        objet.name = nom;
-        objet.somme = sommelettre;
-        // console.log(objet);
-        // console.log("espace");
-        array.push(objet);
+let array = [];
+let trie = "";
+for (let i = 0; i < god.length; i++) {
+	if (god[i] == " ") {
+		// chaque espace de god
+		objet.name = nom;
+		objet.somme = sommelettre;
+		// console.log(objet);
+		// console.log("espace");
+		array.push(objet);
 
-        objet={
-            name: "",
-            somme:"",
-        };
-        sommelettre=0;
-        nom="";
-    }
-    for(let j=0; j<alphabet.length;j++){
-        if(god[i]==alphabet[j]){
-            sommelettre += alphabet.indexOf(god[i])+1; 
-            nom += god[i];
-        //    console.log(sommelettre);
-        }
-    }
+		objet = {
+			name: "",
+			somme: "",
+		};
+		sommelettre = 0;
+		nom = "";
+	}
+	for (let j = 0; j < alphabet.length; j++) {
+		if (god[i] == alphabet[j]) {
+			sommelettre += alphabet.indexOf(god[i]) + 1;
+			nom += god[i];
+			//    console.log(sommelettre);
+		}
+	}
 }
 
-function sortArray(a,b){
-    return a.somme-b.somme;
+function sortArray(a, b) {
+	return a.somme - b.somme;
 }
 array.sort(sortArray);
 console.log(array);
 
-for(let i=0; i<array.length;i++){
-    trie += array[i].name+" ";
+for (let i = 0; i < array.length; i++) {
+	trie += array[i].name + " ";
 }
 console.log(trie);
