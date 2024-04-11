9635ffee6c507480dcad438062eb5a4fb31e66aa
lucas madjinda <lucasmadjinda@gmail.com>
2024-04-11 12:25:35 +0200

    ajout des document examen en version vierge

---
+++ 5_Exercices_En_Cours_Formation/Exo_Algo/Examen_algo_en_version_exo/template_a_renommer_avec_son_prenom_NOM.js
@@ -0,0 +1,156 @@
+/*
+Examen d'algorithmique
+FADDEV31-21-02
+8 novembre 2021
+Nom :
+Prénom :
+*/
+
+/**********************
+Les différents concepts
+**********************/
+
+// Question 1
+
+/*
+Une variable, ...
+*/
+
+// Question 2
+
+// Question 3
+
+// Question 4
+
+// Question 5
+
+// Question 6
+
+// Question 7
+
+// Question 8
+
+// Question 9
+
+// Question 10
+
+// Question 11
+
+// Question 12
+
+// Question 13
+
+// Question 14
+
+// Question 15
+
+// Question 16
+
+// Question 17
+
+// Question 18
+
+// Question 19
+
+// Question 20
+
+// Question 21
+
+// Question 22
+
+// Question 23
+
+// Question 24
+
+// Question 25
+
+/****************************
+TP1 - Les jours de la semaine
+****************************/
+
+// Question 26
+
+// Question 27
+
+// Question 28
+function nameOfDay() {}
+console.log("Question 28", nameOfDay(3));
+
+// Question 29
+
+// Question 30
+function isLeapYear() {}
+console.log("Question 30-1", isLeapYear(2021));
+console.log("Question 30-2", isLeapYear(2020));
+console.log("Question 30-3", isLeapYear(2000));
+console.log("Question 30-4", isLeapYear(1900));
+
+// Question 31
+function nbOfDaysBetweenYears() {}
+console.log("Question 31-1", nbOfDaysBetweenYears(2021, 2021));
+console.log("Question 31-2", nbOfDaysBetweenYears(2020, 2021));
+console.log("Question 31-3", nbOfDaysBetweenYears(2000, 2021));
+
+// Question 32
+function nbOfDaysInYear() {}
+console.log("Question 32", nbOfDaysInYear(2021));
+
+// Question 33
+function numOfMonth() {}
+console.log("Question 33", numOfMonth("novembre"));
+
+// Question 34
+
+// Question 35
+
+// Question 36
+function nbOfDaysSinceNewYear() {}
+console.log("Question 36", nbOfDaysSinceNewYear(8, "novembre", 2021));
+
+// Question 37
+
+// Question 38
+
+// Question 39
+
+// Question 40
+function dayOfTheWeek() {}
+console.log("Question 40", dayOfTheWeek(8, "novembre", 2021));
+
+/*********************
+TP2 - Le jeu de la vie
+*********************/
+
+// Question 41
+
+// Question 42
+
+// Question 43
+
+// Question 44
+
+// Question 45
+
+// Question 46
+
+// Question 47
+function lineToString() {}
+
+// Question 48
+function displayGrid() {}
+
+// Question 49
+function createEmptyArray() {}
+
+// Question 50
+function createEmptyGrid() {}
+
+// Question 51
+function evolution() {}
+
+// Question 52
+
+// Question 53
+function drawGrid() {}
+
+// Question 54
