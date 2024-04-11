8d42b49cb66568e26937e4296db5a2cbfa9313f1
lucas madjinda <lucasmadjinda@gmail.com>
2024-04-11 12:10:31 +0200

    ajout de l'exo algo sur les anniv

---
+++ 5_Exercices_En_Cours_Formation/Exo_Algo/anniv_exo/anniversaires.js
@@ -0,0 +1,109 @@
+/*
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⢤⡼⠋⠙⠓⠲⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠞⠉⠀⠀⣈⣻⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⢠⣿⣁⡀⠀⠀⠀⠀⠙⢷⡀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⢀⣴⣺⠝⠉⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⡟⠁⠘⠻⢷⣤⡀⠀⠀⠀⠙⣆⠀⠀⠀⠀⢰⠃⠀⠀⢀⡾⠋⠁⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠘⣧⠀⠀⢀⡏⠀⠀⢀⡞⠀⠀⠀⠀⣠⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⣀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠙⢦⡀⠀⠸⡗⠉⢩⡇⠀⣠⠏⠀⠀⣀⣤⠞⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠉⠻⣶⠤⠤⠤⣀⣤⣤⣟⣳⣦⣄⡀⠀⠀⠉⠲⢄⣹⣄⣼⣡⠞⠁⣀⡴⠞⠋⠉⠛⠲⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀
+                    ⢰⢶⣶⣾⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠒⠶⢶⣶⡿⠟⠻⢷⣶⣋⠉⠓⠒⠶⠶⣎⣁⣀⠀⠀⠉⠑⣒⣦⣤⣤⣤⠀
+                    ⢸⡄⠀⠉⠛⠲⠶⣶⣦⣤⣤⠤⠤⠤⠶⠖⠒⢛⣉⡡⠤⠒⠚⠿⢅⣈⠉⠉⠓⠒⠒⠶⣶⡤⠭⠽⠿⠛⠛⣉⡩⠔⢻⡟
+                    ⠀⡇⠀⠀⠀⠀⠀⠀⢹⠙⠒⠦⣤⣴⡒⠂⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⢤⣀⢀⣀⡩⢷⠆⠐⠉⠉⠀⠀⠀⢸⠃
+                    ⠀⣧⡀⠀⠀⠀⠀⠀⢸⠀⠀⠀⡇⠀⠉⠙⠒⠦⢄⣀⠀⠀⠀⢀⣀⡀⠤⠴⠒⠚⢹⠁⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⢸⠀
+                    ⠀⠈⣿⠛⠲⠤⣄⣀⣸⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠈⠙⡏⠉⠉⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢸⠀⠀⠀⢀⣀⣤⢶⡟⠁
+                    ⠀⠀⣿⠀⠀⠀⠀⠉⢻⠀⠀⠀⣇⡀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⢸⡶⠶⠛⠉⠁⠀⢸⡇⠀
+                    ⠀⠀⢹⡄⠀⠀⠀⠀⠘⡆⠀⠀⡏⠙⠳⠦⣤⣀⠀⠀⢸⡇⠀⠀⠀⢀⣀⣤⡤⠶⡟⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⢸⠇⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⡇⠀⠀⡇⠀⠀⠀⠀⠉⠛⠳⣾⡧⠶⠖⠛⠉⠉⠀⠀⠀⡄⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⣸⠀⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⡇⠀⠀⢧⠀⠀⠀⠀⠀⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⣿⠀⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⢠⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⣿⠀⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⢸⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⣿⠀⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⢸⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⣿⠀⠀
+                    ⠀⠀⢸⡇⠀⠀⠀⠀⠀⢸⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⣿⠀⠀
+                    ⠀⠀⢸⣤⣀⡀⠀⠀⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢠⡇⠀⠀⠀⠀⢀⣀⡿⠀⠀
+                    ⠀⠀⠀⠀⠈⠉⠛⠲⠦⣼⣄⡀⠀⢧⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⢀⣸⣠⡤⠴⠖⠛⠋⠉⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⣦⣄⡀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢀⣠⣤⠾⠗⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⢦⣄⡀⡇⣀⣤⠶⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+Le but de cet exercice est de répondre à la question "Combien de personnes faut-il réunir dans une pièce pour avoir 50% de chance que deux d'entre elles (au moins) aient le même anniversaire ?" de façon expérimentale.
+*/
+
+/*
+1) Créer une fonction qui retourne un nombre au hasard entre 0 et une valeur donnée.
+Exemple : `random(6)` retourne au hasard 0, 1, 2, 3, 4 ou 5
+          `random(100)` retourne au hasard 0, 1, 2, ..., 98, ou 99
+Pour cette fonction, vous aurez probablement besoin d'utiliser deux fonctions déjà existantes :
+La fonction `Math.random`. C'est une fonction qui ne prend rien en entrée, et qui retourne une valeur au hasard entre 0 et 1 (0.??????????).
+La fonction `Math.floor`. C'est une fonction qui prend un nombre en entrée et qui retourne l'arrondi en bas de ce nombre.
+*/
+
+function randomNumber(n) {
+	return Math.floor(Math.random() * n);
+}
+console.log(randomNumber(5));
+
+/*
+2) Créer une fonction `draft` qui prend deux nombres `length` et `limit` en entrée, et qui retourne un tableau de `length` cases contenant des valeurs choisies au hasard entre 0 et `limit`.
+Exemple : `draft(10, 100)` retourne `[22, 38, 15, 2, 48, 14, 81, 50, 27, 16]`.
+*/
+
+function draft(length, limit) {
+	let array = [];
+	for (let i = 0; i < length; i++) {
+		array.push(randomNumber(limit));
+	}
+	return array;
+}
+console.log(draft(5, 50));
+
+/*
+3) Créer une fonction `hasDuplicates` qui prend un tableau en entrée et qui retourne un booléen :
+- true si le tableau contient (au moins) un double ;
+- false si toutes les valeurs du tableau sont uniques.
+*/
+
+function hasDuplicates(t) {
+	for (let i = 0; i < t.length; i++) {
+		for (let j = i + 1; j < t.length; j++) {
+			if (t[i] == t[j]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+console.log(hasDuplicates([1, 4, 5]));
+
+/*
+4) Créer une fonction `experience` qui prend en entrée :
+- un nombre `length`
+- un nombre `limit`
+- un nombre `nbRepeat`
+et qui répète `nbRepeat` fois le fait de créer un nouveau tableau de `length` valeurs entre 0 et `limit`.
+Cette fonction retourne le nombre de fois où il y a eu des doubles.
+
+Exemple :
+Appel de `experience(10, 100, 5)`
+Premier tableau -> [53, 52, 27, 36, 22, 76, 69, 35, 25, 30] -> Pas de double
+Deuxième tableau -> [15, 11, 3, 43, 86, 73, 22, 12, 59, 70] -> Pas de double
+Troisième tableau -> [31, 58, 25, 70, 7, 34, 78, 70, 37, 8] -> Double
+Quatrième tableau -> [91, 4, 9, 30, 25, 76, 61, 43, 44, 20] -> Pas de double
+Cinquième tableau -> [94, 93, 17, 27, 24, 78, 73, 28, 18, 83] -> Pas de double
+Il n'y a eu des doubles que dans un seul tableau, donc la fonction retourne 1.
+*/
+function experience(length, limit, nbRepeat) {
+	let array = [];
+	for (let i = 0; i < nbRepeat; i++) {
+		array = array.concat(draft(length, limit));
+	}
+	return hasDuplicates(array);
+}
+console.log(experience(1, 365.25, 23));
+
+/*
+5) Appeler la fonction `experience` plusieurs fois (avec une valeur assez grande de `nbRepeat`) pour déterminer combien de personnes faut-il réunir dans la pièce pour avoir 50% que deux d'entre elles partagent une même date d'anniversaire.
+Est-ce que le résultat est cohérent avec ce que vous trouvez sur internet en cherchant "paradoxe des anniversaires" ?
+*/
+//  oui le résultat est cohérent avec ce que on trouve sur internet!
