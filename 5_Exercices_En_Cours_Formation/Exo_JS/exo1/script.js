53d5af2b9211fbbaae3c5e629acf771fe27c26a9
lucas madjinda <lucasmadjinda@gmail.com>
2024-05-16 11:44:56 +0200

    fonction fléché ajouter à une variable puis utiliser dans la fonction for each pour les tableau

--- 5_Exercices_En_Cours_Formation/Exo_JS/exo1/script.js
+++ 5_Exercices_En_Cours_Formation/Exo_JS/exo1/script.js
@@ -1,9 +1,34 @@
-// TODO: Créer une fonction qui reçoit un tableau de 3 notes et qui calcule une moyenne
-// entre ces 3 notes
-// TODO: Dans cette ƒ°, SI la moyenne est supérieure ou égale à 15 on renvoi une string
-// (très Bien)
-// TODO: Dans cette ƒ°, SINON SI la moyenne est supérieure ou égale à 10 on renvoi une
-// string (assez Bien)
-// TODO: Dans cette ƒ°, SINON renvoi une string (refus)
+//TODO: Créer 1 variable pour un nom,
+//TODO: Créer une variable pour un âge,
+//TODO: Créer une variable passions qui est un tableau qui contient 2 chaînes de
+// caractères (au choix)
+//TODO: Puis créer une variable tabUser qui est un tableau qui contient les variable du
+// nom, de l'âge et passions
+//TODO: en Console on affiche le tabUser
+//TODO : en passant par tabUser on veut afficher en console uniquement les passions
+//TODO : en passant par tabUser on veut afficher en console uniquement la 2ème passion
 
-function moyenne(n1, n2, n3) {if ((n1 + n2 + n3) / 3 > 15) {return "très bien";}return "refus";}console.log(moyenne(10, 16, 20));
+// / TODO : Reprendre l'exercice 2 (⁠javascript⁠) en mode objet
+// (un objet user avec des propriétés pour le nom age et passions
+// qui est lui aussi un objet avec 2 propriétés
+
+let moi = {
+	nom: "lucas",
+	age: 21,
+	passion: [
+		{
+			hobbies: "escalade",
+		},
+		{
+			hobbies: "plonger",
+		},
+	],
+};
+
+console.log(moi);
+console.log(moi.passion[1].hobbies);
+let array = [1, 2, 3];
+let test = (element) => {
+	console.log(element);
+};
+array.forEach(test);
