43ef9e81c0806d45891301d7faf10c101085c965
Madjinda Lucas <lucasmadjinda@gmail.com>
2024-07-15 16:48:24 +0200

    ajout de smarties

---
+++ 5_Exercices_En_Cours_Formation/Exo_Algo/smarties/script.js
@@ -0,0 +1,20 @@
+// Le but de cet exercice est de répondre à quelques questions concernant mes derniers relevés de Smarties®.
+
+// Dans une boîte de Smarties®, il y a des bonbons de 8 couleurs différentes : rose, rouge, orange, jaune, vert, bleu, violet, marron
+// Dans ma première boîte, j'ai eu 3, 6, 7, 3, 6, 2, 2 et 1 bonbons de chaque couleur, respectivement
+// Comment pourrais-je stocker cette information là en js ?
+
+// Comment pourrais-je stocker toutes les informations du fichier smarties.data ?
+
+// Maintenant que j'ai ces données :
+
+// Combien y a-t-il de Smarties® dans chaque boîte ?
+// Quel est le nombre minimum de Smarties® dans une boîte ?
+// Et le nombre maximum ?
+// Combien y a-t-il de Smarties® bleus en tout ?
+// Pour chaque couleur, compter le nombre de Smarties® de cette couleur.
+// Quelle est la couleur la plus présente, en tout ?
+// Quel est le nombre maximum de Smarties® d'une seule couleur, dans une boîte ?
+// Dans combien de boîtes y' a-t-il 3 Smarties® bleues ?
+// Dans combien de boîte une couleur est absente ?
+// Dans combien de boîte les Smarties® oranges sont-ils les plus nombreux ?
