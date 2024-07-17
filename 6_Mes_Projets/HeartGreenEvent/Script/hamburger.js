a08e116567589a7c4c327eb47c530b9c7ebbb1ec
lucas madjinda <lucasmadjinda@gmail.com>
2024-07-17 15:12:35 +0200

    modification des fichier script ainsi que les balise scripts dans les différent fichier html

---
+++ 6_Mes_Projets/HeartGreenEvent/Script/hamburger.js
@@ -0,0 +1,59 @@
+// Hamburger Menu
+// Initial state
+const main = document.querySelector("main");
+const nav = document.querySelector(".navLiens");
+const hamburgerIcon = document.querySelector(".hamburgerIcon");
+const crossButton = document.querySelector(".crossButton");
+nav.style.display = "none";
+
+// Behavior and logic of the Hamburger menu
+// Show the menu
+hamburgerIcon.addEventListener("click", () => {
+	// Hide the main content after 500ms
+	setTimeout(() => {
+		main.style.display = "none";
+	}, 500);
+	// Show the navigation menu
+	nav.style.display = "flex";
+	nav.style.animation = "slideInMobile 0.5s forwards";
+});
+
+// Hide the menu
+crossButton.addEventListener("click", () => {
+	// Show the main content
+	main.style.display = "flex";
+	nav.style.animation = "slideOutMobile 0.5s forwards";
+	// Hide the navigation menu after 500ms
+	setTimeout(() => {
+		nav.style.display = "none";
+	}, 500);
+});
+
+// Nav desktop display by default
+const iconUser = document.querySelector("#iconUtilisateur");
+if (window.innerWidth >= 641) {
+	nav.style.display = "flex";
+	iconUser.src = "../Icons/user_icon_white.svg";
+}
+
+// Weird window size matters
+if (window.innerWidth < window.innerHeight) {
+	iconUser.src = "../Icons/user_icon_brown.svg";
+	nav.style.display = "none";
+}
+
+window.addEventListener("resize", () => {
+	if (window.innerWidth >= 641) {
+		nav.style.display = "flex";
+		main.style.display = "flex";
+		iconUser.src = "../Icons/user_icon_white.svg";
+	} else {
+		main.style.display = "flex";
+		nav.style.display = "none";
+		iconUser.src = "../Icons/user_icon_brown.svg";
+	}
+	if (window.innerWidth < window.innerHeight) {
+		iconUser.src = "../Icons/user_icon_brown.svg";
+		nav.style.display = "none";
+	}
+});
