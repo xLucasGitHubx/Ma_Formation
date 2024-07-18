426c2a61996bb000bf62db6d33957f8e2cdf9cad
lucas madjinda <lucasmadjinda@gmail.com>
2024-07-18 16:58:04 +0200

    debut du projet task

---
+++ 5_Exercices_En_Cours_Formation/PHP/Projet _task/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" name="name_user" id="">
+        <input type="text" name="first_name_user" id="">
+        <input type="text" name="login_user" id="">
+        <input type="password" name="mdp_user" id="">
+        <input type="submit" value="ajouter" name="submit">
+    </form>
+    <?php include "./task.php" ?>
+</body>
+
+</html>
---
+++ 5_Exercices_En_Cours_Formation/PHP/Projet _task/task.php
@@ -0,0 +1,40 @@
+<?php
+$name_user = "";
+$first_name_user = "";
+$login_user = "";
+$mdp_user = "";
+
+if (isset($_POST["submit"])) {
+
+    if (
+        isset($_POST["name_user"]) && !empty($_POST["name_user"]) &&
+        isset($_POST["first_name_user"]) && !empty($_POST["first_name_user"]) &&
+        isset($_POST["login_user"]) && !empty($_POST["login_user"]) &&
+        isset($_POST["mdp_user"]) && !empty($_POST["mdp_user"])
+    ) {
+        $name_user = htmlentities(strip_tags(stripcslashes(trim($_POST["name_user"]))));
+        $first_name_user = htmlentities(strip_tags(stripcslashes(trim($_POST["first_name_user"]))));
+        $login_user = htmlentities(strip_tags(stripcslashes(trim($_POST["login_user"]))));
+        $mdp_user = htmlentities(strip_tags(stripcslashes(trim($_POST["mdp_user"]))));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
