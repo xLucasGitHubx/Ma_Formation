e6ab918818e4281fddd6327dbe751a5673cba4dd
lucas madjinda <lucasmadjinda@gmail.com>
2024-07-31 11:54:39 +0200

    ajout fonctionnalités js pour le switch des bouton événement et discussion dans page utilisateur

--- 6_Mes_Projets/HeartGreenEvent/Script/utilisateur.js
+++ 6_Mes_Projets/HeartGreenEvent/Script/utilisateur.js
@@ -1,3 +1,12 @@
+let evenement = document.querySelector(".evenement");
 let discussion = document.querySelector(".discussion");
-discussion.style.display = "none";
-discussion.style.display = "flex";
+let h3 = document.querySelectorAll(".noSelectH3");
+
+h3[0].addEventListener("click", () => {
+	evenement.style.display = "none";
+	discussion.style.display = "flex";
+});
+h3[1].addEventListener("click", () => {
+	discussion.style.display = "none";
+	evenement.style.display = "flex";
+});
