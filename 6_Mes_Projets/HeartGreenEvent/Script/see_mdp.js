3b43e65c3b72eb663c83708ed62b9dddb86217a7
Madjinda Lucas <lucasmadjinda@gmail.com>
2024-08-12 16:33:39 +0200

    ajout du script pour icon mdp vu pas vu et modification de la page utilisateur pou le desktop

---
+++ 6_Mes_Projets/HeartGreenEvent/Script/see_mdp.js
@@ -0,0 +1,12 @@
+let icon = document.querySelector("#password");
+let touchIcon = document.querySelector(".seeMdp");
+
+touchIcon.addEventListener("click", () => {
+	if (icon.type == "password") {
+		icon.style.backgroundImage = "url('../Icons/eyes_see.svg')";
+		icon.type = "text";
+	} else {
+		icon.style.backgroundImage = "url('../Icons/eyes_hide.svg')";
+		icon.type = "password";
+	}
+});
